67a6bb2c22b69af7f87268ecaa3363749e75bfe5
Keysaw <florian@brickx.fr>
2023-05-26 16:38:29 +0200

    Updated TestCase.php & phpunit.xml.dist

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,9 @@
 namespace Brickx\Filament\Maintenance\Tests;
 
 use Brickx\Filament\Maintenance\MaintenanceServiceProvider;
+use Filament\FilamentServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -23,13 +25,15 @@ class TestCase extends Orchestra
 		parent::setUp();
 
 		Factory::guessFactoryNamesUsing(
-			fn (string $modelName) => 'Brickx\\Filament\Maintenance\\Database\\Factories\\'.class_basename($modelName).'Factory'
+			fn (string $modelName) => 'Brickx\\Filament\\Maintenance\\Database\\Factories\\'.class_basename($modelName).'Factory'
 		);
 	}
 
 	protected function getPackageProviders($app) : array
 	{
 		return [
+			LivewireServiceProvider::class,
+			FilamentServiceProvider::class,
 			MaintenanceServiceProvider::class,
 		];
 	}
