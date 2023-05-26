c4fa49feb256f3f322fa52c9dd6cb8ddda3a076b
Keysaw <florian@brickx.fr>
2023-05-26 11:30:08 +0200

    Updated config file

--- config/filament-maintenance.php
+++ config/filament-maintenance.php
@@ -1,15 +1,17 @@
 <?php
 
-// config for Brickx/LaravelFilamentMaintenance
 return [
 
 	/*
 	|--------------------------------------------------------------------------
 	| Secret Token
 	|--------------------------------------------------------------------------
-	| This token will be used to allow access to the application while it is
-	| in maintenance mode. You can set it to any string you want. If set,
+	| This token will be used to allow access to the application while in
+	| maintenance mode. You can set it to any string you want. If set,
 	| it will then be passed to the Artisan ‘down’ command.
+	|
+	| Users will then be able to bypass the maintenance mode by visiting
+	| the following URL: https://your-domain.test/{secret}
 	|
 	*/
 
