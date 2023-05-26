36a3afef84432dc983c18828390e1477f3f606c7
Keysaw <florian@brickx.fr>
2023-05-26 16:18:44 +0200

    Created service provider

---
+++ src/MaintenanceServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Brickx\Filament\Maintenance;
+
+use Brickx\Filament\Maintenance\Http\Livewire\ToggleMaintenance;
+use Filament\Facades\Filament;
+use Livewire\Livewire;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class MaintenanceServiceProvider extends PackageServiceProvider
+{
+	public function boot() : void
+	{
+		parent::boot();
+
+		Livewire::component('filament-maintenance::toggle-maintenance', ToggleMaintenance::class);
+
+		Filament::serving(function () {
+			Filament::registerRenderHook(
+				'global-search.start',
+				fn () => view('filament-maintenance::toolbar.menu-button')
+			);
+		});
+	}
+
+	public function configurePackage(Package $package) : void
+	{
+		$package->name('laravel-filament-maintenance')
+			->hasConfigFile()
+			->hasTranslations()
+			->hasViews('filament-maintenance');
+	}
+}
