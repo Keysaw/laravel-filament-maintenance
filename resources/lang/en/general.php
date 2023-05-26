7afa3ba9607fb932f49d40bd25ea3b69281a833b
Keysaw <florian@brickx.fr>
2023-05-26 16:18:32 +0200

    Created translations

---
+++ resources/lang/en/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'tooltip' => ':Action maintenance mode',
+	'success' => [
+		'title' => 'Maintenance mode :status',
+		'body' => [
+			'up' => 'Your application is now live!',
+			'down' => 'Secret Token: **:secret**',
+		],
+	],
+	'error' => [
+		'title' => 'Something went wrong. Maintenance mode could not be :status.',
+	],
+	'activate' => 'activate',
+	'deactivate' => 'deactivate',
+	'activated' => 'activated',
+	'deactivated' => 'deactivated',
+	'up' => 'Up',
+	'down' => 'Down',
+];
---
+++ resources/lang/fr/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'tooltip' => ':Action le mode maintenance',
+	'success' => [
+		'title' => 'Mode maintenance :status',
+		'body' => [
+			'up' => 'Votre application est en ligne !',
+			'down' => 'Secret Token: **:secret**',
+		],
+	],
+	'error' => [
+		'title' => 'Une erreur est survenue. Le mode maintenance n\'a pas pu être :status.',
+	],
+	'activate' => 'activer',
+	'deactivate' => 'désactiver',
+	'activated' => 'activé',
+	'deactivated' => 'désactivé',
+	'up' => 'En ligne',
+	'down' => 'En maintenance',
+];
